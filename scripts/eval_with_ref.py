226623f22940575362db29b90a3f68b34f5fe542
Xiang Fu <xiangfu@xiangfu.local>
2020-03-16 23:12:52 -0400

    add option to remove duplicates

--- scripts/eval_with_ref.py
+++ scripts/eval_with_ref.py
@@ -3,16 +3,17 @@ import pandas as pd
 
 from argparse import ArgumentParser
 
-def eval_with_ref(pred_path, ref_path, save_path):
+def eval_with_ref(pred_path, ref_path, save_path, unique_smiles=True):
     """Evaluate the predicted compounds <pred_path> by finding which are included in the reference <ref_path>.
     
     Args:
-      pred_path: path to the the prediction .csv file.
-      ref_path: path to the reference .csv file.
-      save_path: path to save the output .csv file.
+      pred_path:      path to the the prediction .csv file.
+      ref_path:       path to the reference .csv file.
+      save_path:      path to save the output .csv file.
+      unique_smiles:  if true, only keep one row if there are entries with the same SMILES
       
     Returns:
-      rows in the prediction csv that overlap with the reference.
+      csv file of rows in the prediction csv that overlap with the reference. saved to <save_path>.
       
     """
     
@@ -20,7 +21,11 @@ def eval_with_ref(pred_path, ref_path, save_path):
     refs = pd.read_csv(ref_path)
     effective = list(
         map((lambda x: x in list(refs['smiles'])), preds['smiles']))
-    output = preds[effective]
+    output = preds[effective].copy()
+
+    if unique_smiles:
+      output.drop_duplicates('smiles', keep='first', inplace=True)
+      
     output.to_csv(save_path, index=False)
     
     
@@ -32,6 +37,8 @@ if __name__ == '__main__':
                         help='path to the reference .csv file.')
     parser.add_argument('--save_path', type=str, required=True,
                         help='path to save the output .csv file.')
+    parser.add_argument('--unique_smiles', action='store_true', default=True,
+                        help='if true, only keep one row if there are entries with the same SMILES')
     args = parser.parse_args()
 
     eval_with_ref(**vars(args))
