8a89f8682014957f583429caabf1ebf4ee49ba09
Kyle Swanson <swansonk.14@gmail.com>
2020-03-27 20:26:21 -0400

    Add auc eval script

---
+++ scripts/evaluate_auc.py
@@ -0,0 +1,21 @@
+from argparse import ArgumentParser
+
+import pandas as pd
+from sklearn.metrics import roc_auc_score
+
+
+def evaluate_auc(true_path: str, pred_path: str):
+    true, pred = pd.read_csv(true_path), pd.read_csv(pred_path)
+    auc = roc_auc_score(true['activity'], pred['activity'])
+    print(f'AUC = {auc}')
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--true_path', type=str, required=True,
+                        help='Path to true .csv file')
+    parser.add_argument('--pred_path', type=str, required=True,
+                        help='Path to predictions .csv file')
+    args = parser.parse_args()
+
+    evaluate_auc(**vars(args))
