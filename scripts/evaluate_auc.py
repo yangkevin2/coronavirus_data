49361146aaf8c458ef21093be796360d9d5df560
Kyle Swanson <swansonk.14@gmail.com>
2020-03-30 15:33:00 -0400

    Adding prc-auc to evaluation

--- scripts/evaluate_auc.py
+++ scripts/evaluate_auc.py
@@ -1,14 +1,29 @@
 from argparse import ArgumentParser
+from typing import List
 
 import pandas as pd
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import auc, precision_recall_curve, roc_auc_score
+
+
+def prc_auc_score(targets: List[int], preds: List[float]) -> float:
+    """
+    Computes the area under the precision-recall curve.
+
+    :param targets: A list of binary targets.
+    :param preds: A list of prediction probabilities.
+    :return: The computed prc-auc.
+    """
+    precision, recall, _ = precision_recall_curve(targets, preds)
+    return auc(recall, precision)
 
 
 def evaluate_auc(true_path: str, pred_path: str):
     true, pred = pd.read_csv(true_path), pd.read_csv(pred_path)
     assert (true['smiles'] == pred['smiles']).all()
-    auc = roc_auc_score(true['activity'], pred['activity'])
-    print(f'AUC = {auc}')
+    roc_auc = roc_auc_score(true['activity'], pred['activity'])
+    prc_auc = prc_auc_score(true['activity'], pred['activity'])
+    print(f'ROC-AUC = {roc_auc}')
+    print(f'PRC-AUC = {prc_auc}')
 
 
 if __name__ == '__main__':
